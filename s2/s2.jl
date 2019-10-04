f55c0070f089e63d064a8916a62414bd98a2d8b5
Parham Alvani <parham.alvani@gmail.com>
2019-10-04 23:57:27 +0330

    feat: Add Session 2 Homework

---
+++ s2/s2.jl
@@ -0,0 +1,21 @@
+using JuMP
+using GLPK
+using GLPKMathProgInterface
+
+m = Model()
+@variable(m, x[1:4] >= 0)
+@variable(m, y[1:4] >= 0)
+@constraint(m, x[1] + y[1] + x[4] + y[4] + y[3] >= 12)
+@constraint(m, x[2] + y[2] + x[1] + y[1] + y[4] >= 8)
+@constraint(m, x[3] + y[3] + x[2] + y[2] + y[1] >= 6)
+@constraint(m, x[4] + y[4] + x[3] + y[3] + y[2] >= 15)
+@objective(m, Min, sum(x[i] * 48 for i=1:4) + sum(y[i] * 84 for i=1:4))
+println(m)
+
+@time optimize!(m, with_optimizer(GLPK.Optimizer))
+
+println("Objective value: ", objective_value(m))
+X = value.(x)
+Y = value.(y)
+println("X: ", X)
+println("Y: ", Y)
