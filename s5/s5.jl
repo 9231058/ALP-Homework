570109f896299126f0df469fec809e9bc9fca7fc
Parham Alvani <parham.alvani@gmail.com>
2019-10-07 11:33:45 +0330

    chore: Add Variable for 1 -> 2 -> 3 in Session 5 Homework

--- s5/s5.jl
+++ s5/s5.jl
@@ -3,14 +3,14 @@ using GLPK
 using GLPKMathProgInterface
 
 m = Model()
-@variable(m, x[0:5] >= 0)
-@constraint(m, x[0] * 2 + x[3] * 2 + x[4] * 3 + x[5] <= 25000)
+@variable(m, x[0:6] >= 0)
+@constraint(m, x[0] * 2 + x[3] * 2 + x[4] * 3 + x[5] + x[6] * 3 <= 25000)
 @constraint(m, x[1] <= 5000)
 @constraint(m, x[2] + x[3] <= 5000)
-@constraint(m, x[4] + x[5] <= 3000)
-@constraint(m, 3 * x[0] == x[1] + x[3] + x[4])
+@constraint(m, x[4] + x[5] + x[6] <= 3000)
+@constraint(m, 3 * x[0] == x[1] + x[3] + x[4] + x[6])
 @constraint(m, x[0] == x[2] + x[5])
-@objective(m, Max, x[1] * 10 + x[2] * 20 + x[3] * 20 + x[4] * 30 + x[5] * 30 - x[0] * 25 - x[2] - x[3] - x[4] * 2 - x[5] * 6)
+@objective(m, Max, x[1] * 10 + x[2] * 20 + x[3] * 20 + x[4] * 30 + x[5] * 30 + x[6] * 30 - x[0] * 25 - x[2] - x[3] - x[4] * 2 - x[5] * 6 - x[6] * 7)
 println(m)
 
 @time optimize!(m, with_optimizer(GLPK.Optimizer))
