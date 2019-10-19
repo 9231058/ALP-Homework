5e2857160b2e7753d917e2461f020efab556bac9
Parham Alvani <parham.alvani@gmail.com>
2019-10-19 18:06:08 +0330

    fix: Correct Session 4 Homework

--- s4/s4.jl
+++ s4/s4.jl
@@ -7,7 +7,7 @@ m = Model()
 @constraint(m, [i=1:3], x[i] <= 0.5)
 @constraint(m, [i=1:3], x[i] >= 0.2)
 @constraint(m, sum(x[i] for i=1:3) <= 1)
-@constraint(m, x[1] * 0.17 + x[2] * 0.27 + x[3] * 0.07 == 0.15)
+@constraint(m, x[1] * 19.5 + x[2] * 27.35 + x[3] * 1.75 == 15)
 @objective(m, Max, x[1] * 10.9 + x[2] * 16.9 + x[3] * 7.4)
 println(m)
 
